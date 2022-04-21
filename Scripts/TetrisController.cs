d59870966c127574b3ad0a02f17dfa86762156ce
root2 <2rootcom4@gmail.com>
2022-04-21 14:58:33 +0900

    feat: Add main controller

---
+++ Scripts/TetrisController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisController : MonoBehaviour
+{
+    [Header("Objects & Prefabs")]
+    [SerializeField] private GameObject blockPrefab;
+
+    [Header("Game Settings")]
+    [SerializeField, Range(4, 40)] private int stageWidth = 10;
+    [SerializeField, Range(5, 20)] private int stageHeight = 20;
+    [SerializeField] private float fallCycle = 1.0f;
+
+    private void Start()
+    {
+        if (!blockPrefab) { Debug.LogError("Error: Prefab for block is missing"); }
+
+        // TODO: border, stage setup
+    }   
+}
+
+public interface IBackgroundSetting
+{
+    void SetupBackground(GameObject a_blockPrefab, int a_stageWidth, int a_stageHeight);
+}
